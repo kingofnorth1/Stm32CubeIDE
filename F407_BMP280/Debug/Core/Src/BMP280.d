371c2a2d4a8704dc395a6fac60ef37af4e16204b
kingofnorth1 <3441423164@qq.com>
2024-06-20 03:12:11 +0800

    学习FreeRTOS系统

--- F407_BMP280/Debug/Core/Src/BMP280.d
+++ F407_BMP280/Debug/Core/Src/BMP280.d
@@ -27,7 +27,8 @@ Core/Src/BMP280.o: ../Core/Src/BMP280.c ../Core/Inc/BMP280.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ ../Core/Inc/usart.h
 ../Core/Inc/BMP280.h:
 ../Core/Inc/i2c.h:
 ../Core/Inc/main.h:
@@ -59,3 +60,4 @@ Core/Src/BMP280.o: ../Core/Src/BMP280.c ../Core/Inc/BMP280.h \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+../Core/Inc/usart.h:
