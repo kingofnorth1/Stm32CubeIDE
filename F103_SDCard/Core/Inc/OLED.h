8fef1b785389fbe64f719484ab7c4ab6f3d7207e
kingofnorth1 <3441423164@qq.com>
2024-10-20 18:50:44 +0800

    添加SDCard

---
+++ F103_SDCard/Core/Inc/OLED.h
@@ -0,0 +1,19 @@
+#include "stdint.h"
+#include "stm32f1xx_hal.h"
+
+#ifndef __OLED_H
+#define __OLED_H
+
+void OLED_Init(void);
+void OLED_Clear(void);
+void OLED_ShowChar(uint8_t Line, uint8_t Column, char Char);
+void OLED_ShowString(uint8_t Line, uint8_t Column, char *String);
+void OLED_ShowNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+void OLED_ShowSignedNum(uint8_t Line, uint8_t Column, int32_t Number, uint8_t Length);
+void OLED_ShowHexNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+void OLED_ShowBinNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length);
+void OLED_6_7_Init(void);
+void OLED_ShowChinese(uint8_t Line, uint8_t Column, uint8_t num);
+void OLED_ClearArea(uint8_t StartLine, uint8_t EndLine, uint8_t StartColumn, uint8_t EndColumn);
+
+#endif
